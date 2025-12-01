0d00bee3cd4acc98455ea9193551ceed25d68396
KudoAkinichi <arookrishna@gmail.com>
2025-12-01 23:19:56 +0530

    Project Initialization

---
+++ flight-service/src/main/java/com/flightapp/flight/FlightServiceApplication.java
@@ -0,0 +1,15 @@
+package com.flightapp.flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableReactiveMongoAuditing
+public class FlightServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlightServiceApplication.class, args);
+    }
+}
